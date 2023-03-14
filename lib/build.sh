97e6c72a1e3e579ddcabb1d3005f1c70aff09630
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2023-03-14 08:25:00 +0100

    Update libraries.

--- lib/build.sh
+++ lib/build.sh
@@ -1,12 +1,35 @@
 #! /bin/bash
 
-# Setup...
-target=$(rm -rf build && mkdir -p build && cd build && pwd)
-threads=$(cat /proc/cpuinfo | grep processor | wc -l)
+# ----------------------------------------------------------------------
+function infomsg {
+# ----------------------------------------------------------------------
+    
+    echo
+    echo "============================================================"
+    echo "Compile: $1"
+    echo "============================================================"
+    echo
+}
+
+# ----------------------------------------------------------------------
+# Main...
+# ----------------------------------------------------------------------
+
+# Check arguments...
+if [ $# -ne 1 ] ; then
+    echo "usage: $0 <target-dir>"
+    exit
+fi
+
+# Set number of cores...
+THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+# Get absolute path...
+target=$(mkdir -p $1 && cd $1 && pwd)
 
 # Prepare directories...
-mkdir -p $target/src $target/bin $target/include $target/lib $target/man/man1 \
-    && cp *tar.bz2 $target/src \
+mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
+    && cp *tar.gz $target/src \
     && cd $target/src \
     && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
     || exit
@@ -45,6 +68,14 @@ cd $target/src/$dir \
     && make -j$THREADS && make check && make install \
     || exit
 
+## HDF4...
+#dir=HDF4.2r4_fix
+#infomsg $dir
+#cd $target/src/$dir \
+#    && ./configure --prefix=$target --disable-netcdf --disable-fortran --disable-hdf4-xdr \
+#    && make -j$THREADS && make check && make install \
+#    || exit
+
 # HDF-EOS...
 infomsg hdfeos
 cd $target/src/hdfeos && bin/INSTALL-HDFEOS <<EOF
@@ -55,34 +86,17 @@ cp include/* $target/include
 cp lib/linux/* $target/lib
 
 # AIRS reader...
-dir=airs-v6
-cd $target/src/$dir \
-    && cp libairs.* $target/lib \
-    && cp airs*.h $target/include
-
-# GSL...
-dir=gsl-2.7
-cd $target/src/$dir \
-    && ./configure --prefix=$target \
-    && make -j$threads && make check && make install && make clean \
-	|| exit
-
-# HDF5...
-dir=hdf5-1.12.1
-cd $target/src/$dir \
-    && ./configure --prefix=$target --with-zlib=$target --enable-hl \
-    && make -j$threads ; make -j$threads && make check && make install && make clean \
-	|| exit
+infomsg airs-v6
+cd $target/src/airs-v6 \
+    && export CFLAGS=-I$target/include \
+    && make \
+    && cp *.h $target/include \
+    && cp lib* $target/lib
 
 # netCDF...
-dir=netcdf-c-4.8.1
+dir=netcdf-4.1.2
+infomsg $dir
 cd $target/src/$dir \
-    && CPPFLAGS=-I$target/include LDFLAGS=-L$target/lib ./configure --prefix=$target --disable-dap --disable-nczarr \
-    && make -j$threads && make check && make install && make clean \
-	|| exit
-
-# Summary...
-echo -e "\n***** gsl-config *****\n"
-$target/bin/gsl-config --libs --cflags --version
-echo -e "\n***** nc-config *****"
-$target/bin/nc-config --all
+    && ./configure --prefix=$target --enable-c-only --disable-dap \
+    && make -j$THREADS && make check && make install \
+    || exit
