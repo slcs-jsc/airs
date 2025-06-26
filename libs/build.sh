bb39b361c6581d328ab5895ed8947cbe34f90b94
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-06-26 22:50:46 +0200

    Update of library build script.

--- libs/build.sh
+++ libs/build.sh
@@ -28,7 +28,7 @@ THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
 target=$(mkdir -p $1 && cd $1 && pwd)
 
 # Prepare directories...
-mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
+mkdir -p $target/src $target/bin $target/include $target/lib $target/man/man1 \
     && cp *tar.gz $target/src \
     && cd $target/src \
     && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
@@ -41,6 +41,7 @@ cd $target/src/$dir \
     && ./configure --prefix=$target \
     && make && make check && make install \
     && cp $target/src/$dir/libjpeg.a $target/lib \
+    && cp $target/src/$dir/*.h $target/include \
     || exit
 
 # zlib...
@@ -72,17 +73,9 @@ dir=hdf-4.2.16
 infomsg $dir
 cd $target/src/$dir \
     && ./configure --prefix=$target --disable-netcdf --disable-fortran \
-		   --disable-hdf4-xdr \
+		   --disable-hdf4-xdr --with-jpeg=$target \
     && make -j$THREADS && make check && make install \
     || exit
-
-## HDF4...
-#dir=HDF4.2r4_fix
-#infomsg $dir
-#cd $target/src/$dir \
-#    && ./configure --prefix=$target --disable-netcdf --disable-fortran --disable-hdf4-xdr \
-#    && make -j$THREADS && make check && make install \
-#    || exit
 
 # HDF-EOS...
 infomsg hdfeos
