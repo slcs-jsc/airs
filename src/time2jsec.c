b1bed79dead7195ed0d75cca20f49b3c2fe9030d
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-06-26 21:10:35 +0200

    Added jsec2time and time2jsec tools.

---
+++ src/time2jsec.c
@@ -0,0 +1,51 @@
+/*
+  This file is part of JURASSIC.
+  
+  JURASSIC is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  JURASSIC is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with JURASSIC. If not, see <http://www.gnu.org/licenses/>.
+  
+  Copyright (C) 2003-2025 Forschungszentrum Juelich GmbH
+*/
+
+/*! 
+  \file
+  Convert date to Julian seconds.
+*/
+
+#include "jurassic.h"
+
+int main(
+  int argc,
+  char *argv[]) {
+
+  double jsec;
+
+  /* Check arguments... */
+  if (argc < 8)
+    ERRMSG("Give parameters: <year> <mon> <day> <hour> <min> <sec> <remain>");
+
+  /* Read arguments... */
+  const int year = atoi(argv[1]);
+  const int mon = atoi(argv[2]);
+  const int day = atoi(argv[3]);
+  const int hour = atoi(argv[4]);
+  const int min = atoi(argv[5]);
+  const int sec = atoi(argv[6]);
+  const double remain = atof(argv[7]);
+
+  /* Convert... */
+  time2jsec(year, mon, day, hour, min, sec, remain, &jsec);
+  printf("%.2f\n", jsec);
+
+  return EXIT_SUCCESS;
+}
