73591b6665d8506cc58016aaefe2e93a836c9868
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-12-07 18:55:15 +0100

    Update headers.

--- src/doy2day.c
+++ src/doy2day.c
@@ -1,21 +1,20 @@
 /*
-  This file is part of the AIRS Code Collection.
+  This file is part of JURASSIC.
   
-  the AIRS Code Collections is free software: you can redistribute it
-  and/or modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation, either version 3 of
-  the License, or (at your option) any later version.
+  JURASSIC is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
   
-  The AIRS Code Collection is distributed in the hope that it will be
-  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
+  JURASSIC is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
   
   You should have received a copy of the GNU General Public License
-  along with the AIRS Code Collection. If not, see
-  <http://www.gnu.org/licenses/>.
+  along with JURASSIC. If not, see <http://www.gnu.org/licenses/>.
   
-  Copyright (C) 2019-2025 Forschungszentrum Juelich GmbH
+  Copyright (C) 2003-2025 Forschungszentrum Juelich GmbH
 */
 
 /*! 
@@ -23,7 +22,7 @@
   Convert day of year to date.
 */
 
-#include "libairs.h"
+#include "jurassic.h"
 
 int main(
   int argc,
