554f481571379a05b2182d05768e0c97b36f4be0
lars2015 <lars2015@users.noreply.github.com>
2026-03-14 07:05:46 +0000

    deploy: e4b671d79946267d5f7cdc05505b557c163e2b34

---
+++ doxygen/search/all_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['add_5fatt_0',['add_att',['../libairs_8c.html#a53cff2d41cd32a516867259768bf972b',1,'add_att(int ncid, int varid, const char *unit, const char *long_name):&#160;libairs.c'],['../libairs_8h.html#a53cff2d41cd32a516867259768bf972b',1,'add_att(int ncid, int varid, const char *unit, const char *long_name):&#160;libairs.c']]],
+  ['add_5fvar_1',['add_var',['../libairs_8c.html#a832132b5251582419a6ca58fe3615f1c',1,'add_var(int ncid, const char *varname, const char *unit, const char *longname, int type, int dimid[], int *varid, int ndims):&#160;libairs.c'],['../libairs_8h.html#a832132b5251582419a6ca58fe3615f1c',1,'add_var(int ncid, const char *varname, const char *unit, const char *longname, int type, int dimid[], int *varid, int ndims):&#160;libairs.c'],['../nlte_8c.html#a832132b5251582419a6ca58fe3615f1c',1,'add_var(int ncid, const char *varname, const char *unit, const char *longname, int type, int dimid[], int *varid, int ndims):&#160;nlte.c'],['../retrieval_8c.html#a832132b5251582419a6ca58fe3615f1c',1,'add_var(int ncid, const char *varname, const char *unit, const char *longname, int type, int dimid[], int *varid, int ndims):&#160;retrieval.c']]],
+  ['airs_20code_20collection_2',['AIRS Code Collection',['../index.html',1,'']]],
+  ['airs_5fchan_3',['airs_chan',['../extract_8c.html#acea59a0b9761234f02b04b605a6ad900',1,'extract.c']]],
+  ['airs_5fl1_5ft_4',['airs_l1_t',['../structairs__l1__t.html',1,'']]],
+  ['airs_5fl2_5ft_5',['airs_l2_t',['../structairs__l2__t.html',1,'']]]
+];
---
+++ doxygen/search/all_a.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['l1_5flat_0',['l1_lat',['../structncd__t.html#a0fc6abc8a3a64ee05f9861cf2b3fe25c',1,'ncd_t']]],
+  ['l1_5flon_1',['l1_lon',['../structncd__t.html#a21d53dcad13e61e2b2d48a5301281b5d',1,'ncd_t']]],
+  ['l1_5fnchan_2',['L1_NCHAN',['../retrieval_8c.html#abaedd5f80b7d77f7af8932674fd92d39',1,'L1_NCHAN():&#160;retrieval.c'],['../diff__apr_8c.html#abaedd5f80b7d77f7af8932674fd92d39',1,'L1_NCHAN():&#160;diff_apr.c'],['../libairs_8h.html#abaedd5f80b7d77f7af8932674fd92d39',1,'L1_NCHAN():&#160;libairs.h'],['../nlte_8c.html#abaedd5f80b7d77f7af8932674fd92d39',1,'L1_NCHAN():&#160;nlte.c']]],
+  ['l1_5fntrack_3',['L1_NTRACK',['../diff__apr_8c.html#a4db0fcad7cf01d2f3038e6d7dc24bdbc',1,'L1_NTRACK():&#160;diff_apr.c'],['../libairs_8h.html#a4db0fcad7cf01d2f3038e6d7dc24bdbc',1,'L1_NTRACK():&#160;libairs.h'],['../retrieval_8c.html#a4db0fcad7cf01d2f3038e6d7dc24bdbc',1,'L1_NTRACK():&#160;retrieval.c'],['../nlte_8c.html#a4db0fcad7cf01d2f3038e6d7dc24bdbc',1,'L1_NTRACK():&#160;nlte.c']]],
+  ['l1_5fnu_4',['l1_nu',['../structncd__t.html#aac97a8d1b49a7df6f982cd74f4e90d2a',1,'ncd_t']]],
+  ['l1_5fnxtrack_5',['L1_NXTRACK',['../retrieval_8c.html#a4c793074edd93ab5923c71431bc54f7f',1,'L1_NXTRACK():&#160;retrieval.c'],['../nlte_8c.html#a4c793074edd93ab5923c71431bc54f7f',1,'L1_NXTRACK():&#160;nlte.c'],['../libairs_8h.html#a4c793074edd93ab5923c71431bc54f7f',1,'L1_NXTRACK():&#160;libairs.h'],['../diff__apr_8c.html#a4c793074edd93ab5923c71431bc54f7f',1,'L1_NXTRACK():&#160;diff_apr.c']]],
+  ['l1_5frad_6',['l1_rad',['../structncd__t.html#ab672f718b8eeed6decd0a73b953ce6c2',1,'ncd_t']]],
+  ['l1_5fsat_5flat_7',['l1_sat_lat',['../structncd__t.html#a71d577738f80889c6a6b82d9e9aa3929',1,'ncd_t']]],
+  ['l1_5fsat_5flon_8',['l1_sat_lon',['../structncd__t.html#a7d9d21a8c6b476caa5d160d01c3df485',1,'ncd_t']]],
+  ['l1_5fsat_5fz_9',['l1_sat_z',['../structncd__t.html#aacdf536f5733440b5379062edbe617c9',1,'ncd_t']]],
+  ['l1_5ftime_10',['l1_time',['../structncd__t.html#ae1861d8f275e05ee6f071b48cfecffa0',1,'ncd_t']]],
+  ['l2_5fnlay_11',['L2_NLAY',['../diff__apr_8c.html#a8505af5d2f1a7a9ac9e4f717b4510651',1,'L2_NLAY():&#160;diff_apr.c'],['../libairs_8h.html#a8505af5d2f1a7a9ac9e4f717b4510651',1,'L2_NLAY():&#160;libairs.h'],['../nlte_8c.html#a8505af5d2f1a7a9ac9e4f717b4510651',1,'L2_NLAY():&#160;nlte.c'],['../retrieval_8c.html#a8505af5d2f1a7a9ac9e4f717b4510651',1,'L2_NLAY():&#160;retrieval.c']]],
+  ['l2_5fntrack_12',['L2_NTRACK',['../retrieval_8c.html#ac24b37360f5cda7de2e658247cb9733d',1,'L2_NTRACK():&#160;retrieval.c'],['../nlte_8c.html#ac24b37360f5cda7de2e658247cb9733d',1,'L2_NTRACK():&#160;nlte.c'],['../diff__apr_8c.html#ac24b37360f5cda7de2e658247cb9733d',1,'L2_NTRACK():&#160;diff_apr.c'],['../libairs_8h.html#ac24b37360f5cda7de2e658247cb9733d',1,'L2_NTRACK():&#160;libairs.h']]],
+  ['l2_5fnxtrack_13',['L2_NXTRACK',['../libairs_8h.html#a645d9e34f05cbeab36d50849719faa63',1,'L2_NXTRACK():&#160;libairs.h'],['../diff__apr_8c.html#a645d9e34f05cbeab36d50849719faa63',1,'L2_NXTRACK():&#160;diff_apr.c'],['../nlte_8c.html#a645d9e34f05cbeab36d50849719faa63',1,'L2_NXTRACK():&#160;nlte.c'],['../retrieval_8c.html#a645d9e34f05cbeab36d50849719faa63',1,'L2_NXTRACK():&#160;retrieval.c']]],
+  ['l2_5fp_14',['l2_p',['../structncd__t.html#a85ed4457b889235e2684c2f9c7ddca37',1,'ncd_t']]],
+  ['l2_5ft_15',['l2_t',['../structncd__t.html#a88cbbfe0d2fd822dfb28b01bc77f2b64',1,'ncd_t']]],
+  ['l2_5fz_16',['l2_z',['../structncd__t.html#a3ba0c167cf8b07eadaa70f7d2bb9326e',1,'ncd_t']]],
+  ['lat_17',['lat',['../structwave__t.html#a9ec94f264b82ccf0a3326494def0657b',1,'wave_t::lat()'],['../structretr__t.html#a0d7feb5f18d0cf38adada57cf1a66509',1,'retr_t::lat()'],['../structpert__t.html#afb5c6394498135b477c7032ae48f8a22',1,'pert_t::lat()'],['../structairs__l2__t.html#ac5572a4ade659f58fa9a4c048f48e2da',1,'airs_l2_t::lat()'],['../structairs__l1__t.html#accea09c81f80ba683095cff0827bdac4',1,'airs_l1_t::lat()'],['../structmodel__t.html#a119cdc0d15f1e483980be1b506c63956',1,'model_t::lat()']]],
+  ['libairs_2ec_18',['libairs.c',['../libairs_8c.html',1,'']]],
+  ['libairs_2eh_19',['libairs.h',['../libairs_8h.html',1,'']]],
+  ['lon_20',['lon',['../structwave__t.html#a87d95449252837398ec92205abd6838d',1,'wave_t::lon()'],['../structretr__t.html#a52c67e1f132095b0ea38ac656f7ac82b',1,'retr_t::lon()'],['../structpert__t.html#abb50c5f071bec95fb58ebb0a97a6f207',1,'pert_t::lon()'],['../structairs__l2__t.html#aecd02ad67766869f1f0fcfab0f424293',1,'airs_l2_t::lon()'],['../structairs__l1__t.html#a4a45181a864da76de9db2ec7f6b68510',1,'airs_l1_t::lon()'],['../structmodel__t.html#a57e11ad17fa3d19731a5ec1e3efccbe9',1,'model_t::lon()']]]
+];
